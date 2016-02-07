1219826db22b16489d4d8754d980c5f48fbf30bc
Thomas Chu <itsthomaschu@gmail.com>
2016-02-07 16:42:57 -0700

    Added LandingPage controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'landing_pages/home'
+
+  get 'landing_pages/contact'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
